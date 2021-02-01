aedc9c7d7d311f6ee89094fdd2ab79a556975b5c
MM-HASAN <2020.mmhasan@gmail.com>
2021-02-02 05:40:40 +0600

    true-false practice

---
+++ true-false.js
@@ -0,0 +1,28 @@
+//falsy value:
+// false
+// 0
+//""
+//undefined
+//null
+//Nan
+
+
+// all number +- truthe other then zero
+var age = 0;
+
+if(age){
+  console.log(' this is truc');
+}else{
+  console.log(" this false ")
+}
+
+// any string true other then empty string
+
+var age = "";
+
+if(age.length){
+  console.log(' this is true');
+}else{
+  console.log('this false ')
+
+}
