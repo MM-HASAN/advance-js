93e5a6a16a1a271a2e754b132ffd9197aea94b43
MM-HASAN <2020.mmhasan@gmail.com>
2021-02-02 05:58:31 +0600

    null vs undefined practice

---
+++ undefinedVsNull.js
@@ -0,0 +1,18 @@
+function add(num1, num2){
+  //return num1 + num2;
+}
+
+const num3 = add(3, 9);
+console.log(num3)
+
+//var murad; not defined, show undefined
+// but no return show the undefined
+
+//object property without value return undefined
+const myObj = {
+  name: "murad",
+  age: 59,
+}
+
+console.log(myObj.phone)
+
