bdde30d8c5c46f9e8ca84eff4cc12a405a4ceb0d
MM-HASAN <2020.mmhasan@gmail.com>
2021-02-02 06:33:28 +0600

    equality practice

---
+++ equality.js
@@ -0,0 +1,28 @@
+var myNumber = 2;
+var yourNumber = "2";
+
+if(myNumber === yourNumber){
+  console.log("true")
+}else{
+  console.log("false") 
+}
+
+var myNumber = 2;
+var yourNumber = "2";
+
+if(myNumber == yourNumber){
+  console.log("true")
+}else{
+  console.log("false") 
+}
+
+0 is false so return true
+1 is true so 1 === false is false;
+var myNumber = 0;
+var yourNumber = false;
+
+if(myNumber == yourNumber){
+  console.log("true")
+}else{
+  console.log("false") 
+}
