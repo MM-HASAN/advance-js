f2d3658735f6e1fa46cc2a516c179bf6f2d0bb3d
MM-HASAN <2020.mmhasan@gmail.com>
2021-02-02 07:42:13 +0600

    map practice

---
+++ mapWork.js
@@ -0,0 +1,16 @@
+var nameList = [
+  {namee: 'murad', age:34},
+  {namee: 'rahim', age:94},
+  {namee: 'raxim', age:30},
+  {namee: 'que', age:04},
+]
+
+// const myName = nameList.map( n=>n.namee)
+const myName = nameList.map( n => n.age)
+// console.log(myName);
+
+// const myAge = nameList.filter( n=>n.age)
+// console.log(myAge)
+
+// const finds = nameList.find( n=> n.age>50)
+// console.log(finds)
